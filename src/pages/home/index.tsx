bae3db19f92b4f276b637852431807ffb24ee345
Vulquimar Silva <vuquimar@hotmail.com>
2024-03-03 20:03:02 -0300

    pagina home em construcao

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,36 @@
+import { DealsChart, UpcomingEvents } from "@/components"
+import { Col, Row } from "antd"
 
 export const Home = () => {
   return (
     <div>
-      <h1>HOME - PAREI NO 1:19:45</h1>
+      <Row
+      gutter={[32, 32]}
+      style={{
+        marginTop: '32px'
+      }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          <DealsChart />
+        </Col>
+      </Row>
     </div>
   )
 }
